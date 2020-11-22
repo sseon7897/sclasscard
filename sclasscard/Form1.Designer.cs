a556f532cd20acf3119257a80d879e52be03c153
yvelta1 <61836785+yvelta1@users.noreply.github.com>
2020-11-22 16:37:43 +0900

    배포

---
+++ sclasscard/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿
+namespace sclasscard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gunaTextBox1 = new Guna.UI.WinForms.GunaTextBox();
+            this.gunaTextBox2 = new Guna.UI.WinForms.GunaTextBox();
+            this.gunaTextBox3 = new Guna.UI.WinForms.GunaTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.gunaButton1 = new Guna.UI.WinForms.GunaButton();
+            this.gunaGoogleSwitch1 = new Guna.UI.WinForms.GunaGoogleSwitch();
+            this.label6 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gunaTextBox1
+            // 
+            this.gunaTextBox1.BaseColor = System.Drawing.Color.White;
+            this.gunaTextBox1.BorderColor = System.Drawing.Color.Silver;
+            this.gunaTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.gunaTextBox1.FocusedBaseColor = System.Drawing.Color.White;
+            this.gunaTextBox1.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.gunaTextBox1.FocusedForeColor = System.Drawing.SystemColors.ControlText;
+            this.gunaTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaTextBox1.Location = new System.Drawing.Point(12, 13);
+            this.gunaTextBox1.Name = "gunaTextBox1";
+            this.gunaTextBox1.PasswordChar = '\0';
+            this.gunaTextBox1.Size = new System.Drawing.Size(160, 32);
+            this.gunaTextBox1.TabIndex = 0;
+            // 
+            // gunaTextBox2
+            // 
+            this.gunaTextBox2.BaseColor = System.Drawing.Color.White;
+            this.gunaTextBox2.BorderColor = System.Drawing.Color.Silver;
+            this.gunaTextBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.gunaTextBox2.FocusedBaseColor = System.Drawing.Color.White;
+            this.gunaTextBox2.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.gunaTextBox2.FocusedForeColor = System.Drawing.SystemColors.ControlText;
+            this.gunaTextBox2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaTextBox2.Location = new System.Drawing.Point(12, 51);
+            this.gunaTextBox2.Name = "gunaTextBox2";
+            this.gunaTextBox2.PasswordChar = 'ㆍ';
+            this.gunaTextBox2.Size = new System.Drawing.Size(160, 32);
+            this.gunaTextBox2.TabIndex = 3;
+            // 
+            // gunaTextBox3
+            // 
+            this.gunaTextBox3.BaseColor = System.Drawing.Color.White;
+            this.gunaTextBox3.BorderColor = System.Drawing.Color.Silver;
+            this.gunaTextBox3.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.gunaTextBox3.FocusedBaseColor = System.Drawing.Color.White;
+            this.gunaTextBox3.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.gunaTextBox3.FocusedForeColor = System.Drawing.SystemColors.ControlText;
+            this.gunaTextBox3.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaTextBox3.Location = new System.Drawing.Point(13, 102);
+            this.gunaTextBox3.Name = "gunaTextBox3";
+            this.gunaTextBox3.PasswordChar = '\0';
+            this.gunaTextBox3.Size = new System.Drawing.Size(374, 32);
+            this.gunaTextBox3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(180, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 19);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(180, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 19);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "PW";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 19);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(142, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 19);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Words";
+            // 
+            // gunaButton1
+            // 
+            this.gunaButton1.AnimationHoverSpeed = 0.07F;
+            this.gunaButton1.AnimationSpeed = 0.03F;
+            this.gunaButton1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.gunaButton1.BorderColor = System.Drawing.Color.Black;
+            this.gunaButton1.FocusedColor = System.Drawing.Color.Empty;
+            this.gunaButton1.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaButton1.ForeColor = System.Drawing.Color.White;
+            this.gunaButton1.Image = null;
+            this.gunaButton1.ImageAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.gunaButton1.ImageSize = new System.Drawing.Size(20, 20);
+            this.gunaButton1.Location = new System.Drawing.Point(12, 264);
+            this.gunaButton1.Name = "gunaButton1";
+            this.gunaButton1.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.gunaButton1.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.gunaButton1.OnHoverForeColor = System.Drawing.Color.White;
+            this.gunaButton1.OnHoverImage = null;
+            this.gunaButton1.OnPressedColor = System.Drawing.Color.Black;
+            this.gunaButton1.Size = new System.Drawing.Size(160, 42);
+            this.gunaButton1.TabIndex = 10;
+            this.gunaButton1.Text = "Let\'s do this";
+            this.gunaButton1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.gunaButton1.Click += new System.EventHandler(this.gunaButton1_Click);
+            // 
+            // gunaGoogleSwitch1
+            // 
+            this.gunaGoogleSwitch1.BaseColor = System.Drawing.SystemColors.Control;
+            this.gunaGoogleSwitch1.CheckedOffColor = System.Drawing.Color.DarkGray;
+            this.gunaGoogleSwitch1.CheckedOnColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.gunaGoogleSwitch1.FillColor = System.Drawing.Color.White;
+            this.gunaGoogleSwitch1.Location = new System.Drawing.Point(257, 20);
+            this.gunaGoogleSwitch1.Name = "gunaGoogleSwitch1";
+            this.gunaGoogleSwitch1.Size = new System.Drawing.Size(38, 20);
+            this.gunaGoogleSwitch1.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(213, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 19);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Save";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(16, 233);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 25);
+            this.numericUpDown1.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(180, 263);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 19);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Standby :)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(181, 283);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(84, 19);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Not started.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(399, 361);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.gunaGoogleSwitch1);
+            this.Controls.Add(this.gunaButton1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gunaTextBox3);
+            this.Controls.Add(this.gunaTextBox2);
+            this.Controls.Add(this.gunaTextBox1);
+            this.Font = new System.Drawing.Font("Malgun Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "S-Class Card";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI.WinForms.GunaTextBox gunaTextBox1;
+        private Guna.UI.WinForms.GunaTextBox gunaTextBox2;
+        private Guna.UI.WinForms.GunaTextBox gunaTextBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private Guna.UI.WinForms.GunaButton gunaButton1;
+        private Guna.UI.WinForms.GunaGoogleSwitch gunaGoogleSwitch1;
+        private System.Windows.Forms.Label label6;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ sclasscard/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.IO;
+
+namespace sclasscard
+{
+    public partial class Form1 : Form
+    {
+        string cid, cpw, curl, savedid;
+        decimal word, num;
+        protected ChromeDriverService _driverService = null;
+        protected ChromeOptions _options = null;
+        protected ChromeDriver _driver = null;
+        public Form1()
+        {
+            InitializeComponent();
+            _driverService = ChromeDriverService.CreateDefaultService();
+            _driverService.HideCommandPromptWindow = true;
+
+            _options = new ChromeOptions();
+            _options.AddArgument("disable-gpu");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FileInfo File_Check = new FileInfo(Application.StartupPath + @"\sclassid.tmp");
+
+            if (File_Check.Exists) 
+            {
+                gunaGoogleSwitch1.Checked = true;
+                savedid = System.IO.File.ReadAllText(Application.StartupPath + @"\sclassid.tmp");
+                gunaTextBox1.Text = savedid; // 저장된 아이디가 있다면 불러오기
+            }
+
+            else
+            gunaGoogleSwitch1.Checked = false;
+        }
+
+        private void gunaButton1_Click(object sender, EventArgs e)
+        {
+            if (gunaTextBox1.Text != "")
+            {
+                if (gunaTextBox2.Text != "")
+                {
+                    if (gunaTextBox3.Text != "")
+                    {
+                        cid = gunaTextBox1.Text; // 아이디
+                        cpw = gunaTextBox2.Text; // 비밀번호
+                        curl = gunaTextBox3.Text; // 클래스카드 주소
+
+                        word = numericUpDown1.Value; // 단어 개수
+
+                        label5.Text = "Started.";
+                        label7.Text = "(1/2) Login..";  // 라벨 변경
+
+                        _driver = new ChromeDriver(_driverService, _options);
+                        _driver.Navigate().GoToUrl("http://www.classcard.net/Login"); // 주소값을 텍스트박스의 텍스트로
+                        _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+                        var ids = _driver.FindElementByXPath("/html/body/div[1]/div[2]/div/div/div/form/div[1]/input[1]"); //아이디
+                        ids.SendKeys(cid);
+
+                        var pws = _driver.FindElementByXPath("/html/body/div[1]/div[2]/div/div/div/form/div[1]/input[2]"); //비밀번호
+                        pws.SendKeys(cpw);
+
+                        var logs = _driver.FindElementByXPath("/html/body/div[1]/div[2]/div/div/div/form/div[2]/button"); //로그인
+                        logs.Click();
+
+                        backgroundWorker1.RunWorkerAsync(); //암기 사이트 접속 및 매크로 시작    
+                        
+                        if (gunaGoogleSwitch1.Checked == true) // ID저장 체크 시
+                        {
+                            System.IO.File.WriteAllText(Application.StartupPath + @"\sclassid.tmp", gunaTextBox1.Text); // 같은 폴더에 sclassid.tmp로 아이디 평문 저장
+
+                        }
+                         else
+                        {
+                            System.IO.File.WriteAllText(Application.StartupPath + @"\sclassid.tmp", "");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("모든 내용을 확인하십시오.");
+            }                      
+        }
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            ((IJavaScriptExecutor)_driver).ExecuteScript("window.open();");
+            _driver.SwitchTo().Window(_driver.WindowHandles.Last()); // 로그인 탭에서 암기 탭으로 변경
+
+
+            //클래스카드 암기 주소로 이동
+            _driver.Navigate().GoToUrl(curl);
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5); // 5초 대기
+
+            num = 0;
+            var mean = _driver.FindElementByXPath("/html/body/div[2]/div/div/div[3]/div[1]/a"); // 뜻 보기
+            var know = _driver.FindElementByXPath("/html/body/div[2]/div/div/div[3]/div[2]/a"); // 알아요 버튼
+            var next = _driver.FindElementByXPath("/html/body/div[2]/div/div/div[2]/div[4]/a"); // 다음 단어 버튼
+            var nesett = _driver.FindElementByXPath("/html/body/div[5]/div/div[2]/div[3]/div[3]/a"); // 다음 세트 버튼
+            do
+            {
+                mean.Click(); // 뜻 보기 클릭
+                know.Click(); // 이제 알아요 클릭
+                next.Click(); // 다음 클릭
+                num += 1;
+                if ((num % 10) == 0)
+                {
+                    nesett.Click(); // 다음 클릭
+
+                }
+
+            } while (num <= word);
+
+            // var exit = _driver.FindElementByXPath("/html/body/div[1]/div/div[1]/div[1]/a");
+            // var exit2 = _driver.FindElementByXPath("/html/body/div[13]/div[2]/div/div/a[5]");
+
+            //exit.Click();
+            //exit2.Click();
+            // 아직 구현 안됨.
+
+
+
+            
+
+            
+        }
+    }
+}
